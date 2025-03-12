3c69699aab41d5d69e7f24ea3e83b12759f700a1
Raylaoli <153030786+Raylaoli@users.noreply.github.com>
2025-03-12 20:51:40 +0000

    Criando o projeto

---
+++ primus/src/test/java/com/br/primus/PrimusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.primus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
